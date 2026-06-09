fcee9a6d4bdf5edacb28faf56dcd01019d1656f4
Arman Thakur <yashpalsharma9950@gmail.com>
2026-06-09 19:22:19 +0530

    Initialize Express application with basic route

---
+++ backend/app/app.ts
@@ -0,0 +1,13 @@
+
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+ res.json({ message: "Server is running" });
+});
+
+export default app;
