1e98bcfbcee3bc3743da1b4a317117f38a238feb
Arman Thakur <yashpalsharma9950@gmail.com>
2026-06-10 18:26:21 +0530

    Set up HTTP server with Bun framework

--- backend/server.ts
+++ backend/server.ts
@@ -1,1 +1,12 @@
-console.log("Hello via Bun!");
+
+import http from "http";
+import app from "./app/app";
+
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer(app);
+
+server.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
+
